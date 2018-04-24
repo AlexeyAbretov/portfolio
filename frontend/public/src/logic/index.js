fc54fbf18fb1457bac35f7d936ea7b0a920d2ab9
Alexey Abretov <alexey.abretov@gmail.com>
2018-04-24 16:57:03 +0300

    fixed bugs in sorting method

--- frontend/public/src/logic/index.js
+++ frontend/public/src/logic/index.js
@@ -1,9 +1,11 @@
-export default (a, b) => {
-  let result = ((a.price || 0) - (a.discount || 0)) -
-    ((b.price || 0) - (b.discount || 0));
+export const nameSort = (a, b) => (a.name > b.name ? 1 : -1);
+
+export const priceSort = (a, b) => {
+  let result = ((a.fee || 0) - (a.discount || 0)) -
+    ((b.fee || 0) - (b.discount || 0));
 
   if (result === 0) {
-    result = a.name > b.name ? 1 : -1;
+    result = nameSort(a, b);
   }
 
   return result;
